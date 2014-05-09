c31307379ae4bcc53e5d6478df34d5408bc1517a
Tania <taniuxpc@gmail.com>
2014-05-08 20:33:42 -0700

    modification

---
+++ hw4-137175/src/main/java/edu/cmu/lti/f13/hw4/annotators/DocumentVectorAnnotator.java
@@ -0,0 +1,47 @@
+package edu.cmu.lti.f13.hw4.annotators;
+
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.IntegerArray;
+import org.apache.uima.jcas.cas.StringArray;
+import org.apache.uima.jcas.tcas.Annotation;
+
+import edu.cmu.lti.f13.hw4.typesystems.Document;
+
+@SuppressWarnings("unused")
+public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
+
+	public static Object stopwords;
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+
+		FSIterator<Annotation> iter = jcas.getAnnotationIndex().iterator();
+		if (iter.isValid()) {
+			iter.moveToNext();
+			Document doc = (Document) iter.get();
+			createTermFreqVector(jcas, doc);
+		}
+
+	}
+	/**
+	 * 
+	 * @param jcas
+	 * @param doc
+	 */
+
+	private void createTermFreqVector(JCas jcas, Document doc) {
+
+		String docText = doc.getText();
+		
+		//TO DO: construct a vector of tokens and update the tokenList in CAS
+		
+
+	}
+	public static Object loadStopwords() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
